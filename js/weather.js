3eb7497f9c8eccfeb5e7e68e3f1834a863e266ff
hyeond0 <a98792530@gmail.com>
2023-01-28 09:40:27 +0900

    Create weather, Update all function

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "bf8c3134ef3b94b56d07f4dcb204b6c2";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live it", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
